d192b681b1191804976b546ab9440fa7380c8af6
hanbee <sonhanbi1002@gmail.com>
2025-09-01 23:16:44 +0900

    #feature application 포트 생성

--- src/main/java/tobyspring/splearn/domain/PasswordEncoder.java
+++ src/main/java/tobyspring/splearn/domain/PasswordEncoder.java
@@ -1,5 +1,10 @@
 package tobyspring.splearn.domain;
 
+/**
+ * PasswordEncoder 는 특성이 required application 에 해당 하지만
+ * 의존 방향이 application → domain 으로 되어야 하기 때문에
+ * domain 패키지에 두어도 됨. (domain 도 application 내부이므로)
+ */
 public interface PasswordEncoder {
     String encode(String password);
     boolean matches(String password, String passwordHash);
